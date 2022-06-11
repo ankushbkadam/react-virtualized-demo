1bf4ac6769656dd63dfc30bfc28239e04a12da3f
Ankush Kadam <ankushbkadam@gmail.com>
2022-06-11 21:34:12 +0530

    normal user list rendered

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
+import * as React from "react";
 import "./App.css";
+import { faker } from "@faker-js/faker";
 
 function App() {
-    return <div className="App">React Virtualized</div>;
+    const [users, setUsers] = React.useState([]);
+
+    React.useEffect(() => {
+        findAndSetUserList();
+    }, []);
+
+    const findAndSetUserList = () => {
+        let list = [...Array(100).keys()].map((key) => {
+            return {
+                id: faker.datatype.uuid(),
+                username: faker.internet.userName(),
+                email: faker.internet.email(),
+            };
+        });
+        setUsers(list);
+    };
+
+    return (
+        <div className="App">
+            <div className="users">
+                {users.map((user) => {
+                    return (
+                        <div key={user.id} className="user">
+                            <div className="username">{user.username}</div>
+                            <div className="email">{user.email}</div>
+                        </div>
+                    );
+                })}
+            </div>
+        </div>
+    );
 }
 
 export default App;
